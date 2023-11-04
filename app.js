5bc76642e197d02500074fa51ef569bf41b57660
Khoa <anhkhoa162003@gmail.com>
2023-11-04 15:35:47 +0700

    [+] Post schema + creating a post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connection.on("error", err => {
 const postRoutes = require("./routes/post");
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use("/", postRoutes);
 
 const port = process.env.PORT || 8080;
--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,24 @@
+const Post = require("../models/post");
+
 exports.getPosts = (req, res) => {
   res.json({
     posts: [{ title: "First post" }, { title: "Second post" }],
   });
+};
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+  // console.log("Creating Post: ", req.body);
+  post
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        post: result,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        error: err,
+      });
+    });
 };
