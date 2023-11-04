09a852792dbf49b8470e9f9e0bb91395fa0fbd4e
Khoa <anhkhoa162003@gmail.com>
2023-11-04 16:05:11 +0700

    [+] Validation and friendly error messages

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const dotenv = require("dotenv");
 dotenv.config();
 
+// db
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("DB Connected");
 });
@@ -14,10 +16,13 @@ mongoose.connection.on("error", err => {
   console.log(`DB connection error: ${err.message}`);
 });
 
+// bring in routes
 const postRoutes = require("./routes/post");
 
+// middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(expressValidator());
 app.use("/", postRoutes);
 
 const port = process.env.PORT || 8080;
--- controllers/post.js
+++ controllers/post.js
@@ -9,16 +9,9 @@ exports.getPosts = (req, res) => {
 exports.createPost = (req, res) => {
   const post = new Post(req.body);
   // console.log("Creating Post: ", req.body);
-  post
-    .save()
-    .then((result) => {
-      res.status(200).json({
-        post: result,
-      });
-    })
-    .catch((err) => {
-      return res.status(400).json({
-        error: err,
-      });
+  post.save().then((result) => {
+    res.status(200).json({
+      post: result,
     });
+  });
 };
