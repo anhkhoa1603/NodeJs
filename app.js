2daf967f9027e5cbbff20cd2da65f13a40f62b55
Khoa <anhkhoa162003@gmail.com>
2023-11-04 14:57:58 +0700

    [+] Connect to database: MongoDb

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("DB Connected");
+});
+
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 const postRoutes = require("./routes/post");
 
 app.use(morgan("dev"));
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
